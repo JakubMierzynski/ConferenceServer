5b223b7d94b3d005d20d7a5ab0737e1a2311ec20
jakubmierzynski <jakub.mierzynski@gmail.com>
2023-09-14 16:46:08 +0200

    Added template for html form to create new room Added view to Add new conference

--- ServerConferenceRoom/ServerConferenceRoom/urls.py
+++ ServerConferenceRoom/ServerConferenceRoom/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from ConferenceApp.views import AddNewConferenceRoom
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("room/new/", AddNewConferenceRoom.as_view()),
 ]
